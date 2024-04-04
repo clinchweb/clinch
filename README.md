103db2f55eb74b7bd5d270a6be049cb56836180b
aguslorca <aguslorca.12@gmail.com>
2024-04-04 15:15:19 -0300

    clinch-web v1

---
+++ README.md
@@ -0,0 +1,1 @@
+# ClinchHR
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Navigation from './routes/Navigation'
+import "./animations.css"
+function App() {
+
+  return (
+    <Navigation/>
+  )
+}
+
+export default App
